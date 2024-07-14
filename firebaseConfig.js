a7599737a9d58cae9336961e09ce3c8797c6ea4f
tjf1 <117757363+tjf1dev@users.noreply.github.com>
2024-07-14 16:32:32 +0200

    upload

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+// public/firebaseConfig.js
+var firebaseConfig = {
+    apiKey: "AIzaSyCwRMm-x1Nq77CAOFNcLlrtujniM_Qd8M0",
+    authDomain: "through-the-shadows.firebaseapp.com",
+    databaseURL: "https://through-the-shadows-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "through-the-shadows",
+    storageBucket: "through-the-shadows.appspot.com",
+    messagingSenderId: "545447466617",
+    appId: "1:545447466617:web:45d32ec7cfbc9b1f7fe6a8",
+    measurementId: "G-G7SCGC8E1K"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
